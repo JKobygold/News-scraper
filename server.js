6080ab97af6ceecf442d9ab53e606b8230e420bf
Jacob Gold <jkobygold@gmail.com>
2020-04-27 18:15:00 -0400

    adding starting code

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
